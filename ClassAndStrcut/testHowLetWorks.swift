450eabca4c6b417f4c6e6daa4f7d9b865fd341e4
MartyTheGout <chihyung0@naver.com>
2025-01-06 10:42:53 +0900

    상수로 선언된 구조체와 클래스(상수 프로퍼티를 포함)에 대한 값변경 테스트

--- ClassAndStrcut/testHowLetWorks.swift
+++ ClassAndStrcut/testHowLetWorks.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+// 구조체 : a는 변수 Int, b는 상수 Int
+struct AStruct {
+    var a: Int
+    let b: Int
+}
+
+// 클래스 : a는 변수 Int, b는 상수 Int
+class AClass {
+    var a: Int
+    let b: Int
+    
+    init(a: Int, b: Int) {
+        self.a = a
+        self.b = b
+    }
+}
+
+func testHowLetWorks() {
+
+    let aStruct = AStruct(a: 1, b: 2)
+//    aStruct.a = 3 // Cannot assign to property: 'aStruct' is a 'let' constant
+//    aStruct.b = 4 // Cannot assign to property: 'b' is a 'let' constant
+    
+    let aClass = AClass(a: 1, b: 2)
+    aClass.a = 3
+//    aClass.b = 4 // Cannot assign to property: 'b' is a 'let' constant
+}
